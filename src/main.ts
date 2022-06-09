73def2e604d34a67e3299d1116a298824fd2495f
Tim Hoffmann <hi@tim-hoffmann.net>
2022-06-09 21:07:01 +0200

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { WebModule } from './web/web.module';
+
+function useSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Notification Service')
+    .setDescription(
+      'This is a Notification Service that can be used to send templated notifications.',
+    )
+    .setVersion('1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(WebModule);
@@ -9,6 +24,7 @@ async function bootstrap() {
   app.enableVersioning();
   app.setGlobalPrefix('v1');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  useSwagger(app);
 
   await app.listen(3000, () => logger.log('App listening on port 3000'));
 }
